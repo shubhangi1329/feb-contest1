f6e23a33c496dde3f01c8c7f233df718f1082102
shubhangi1329 <13shubhangi.saraswat@gmail.com>
2023-02-11 16:02:56 +0530

    Update script.js
    final code

--- script.js
+++ script.js
@@ -8,20 +8,46 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+
+ arr.map(function(value){
+  if(value.marks>50){
+    console.log(value);
+  }
+ })
 }
 
 function PrintStudentsbyForEach() {
-  //Write your code here , just console.log
+  //Write your code here , just console.lo
+  arr.forEach(function(value){
+    if(value.marks>50){
+      console.log(value);
+    }
+  })
 }
 
 function addData() {
   //Write your code here, just console.log
+
+  var stud={id:4,name:"susan",age:"20",marks:45}
+  arr.push(stud)
+  console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+
+  arr=arr.filter(function(value){
+    if(value.marks>50)
+    return value;
+  })
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+
+  var arr2=[{id:101,name:"shubhi",age:"20",marks:95}, {id:102, name:"kumud",age:"25", marks:93}, {id:103, name:"siddhi", age:"24",marks:25}]
+  var res=arr.concat(arr2)
+  console.log(res);
+
 }
